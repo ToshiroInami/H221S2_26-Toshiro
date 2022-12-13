b0989f004eef33e80cc343cc247693bfc968ca19
Toshiro Inami <111782241+ToshiroInami@users.noreply.github.com>
2022-12-13 18:25:15 -0500

    landing

---
+++ app.js
@@ -0,0 +1,49 @@
+const fullimg = document.getElementById("fullimg");
+const fulimg = document.getElementById("fulimg");
+
+function openimg(reference){
+    fullimg.style.display="flex";
+    fulimg.src = reference;
+}   
+
+function closeimg(){
+    fullimg.style.display ="none";
+}
+
+/* box */
+
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes);
+
+checkBoxes(); // shows initial box(es) 
+
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5 * 4);
+  
+  boxes.forEach(box => {
+    const boxTop = box.getBoundingClientRect().top;
+    
+    if(boxTop < triggerBottom) {
+      box.classList.add('show');
+    } else {
+      box.classList.remove('show');
+    }
+  })
+}
+
+/*tiempo*/ 
+
+var x = document.querySelector('.app');
+//evento que se repite , llama a un css
+x.addEventListener("webkitAnimationIteration", myRepeatFunction);
+
+
+x.addEventListener("animationiteration", myRepeatFunction);
+
+function myRepeatFunction(event) {
+  this.style.backgroundColor = "lightblue";
+  
+  this.innerHTML = "Tiempo " + event.elapsedTime + " segundos";
+}
+
