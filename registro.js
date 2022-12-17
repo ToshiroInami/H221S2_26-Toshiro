3096be6431f6a1d7da7a000eb01ac2da65089b6b
toshiro <toshiro.inami@vallegrande.edu.pe>
2022-12-16 20:21:11 -0500

    a

--- registro.js
+++ registro.js
@@ -1,4 +1,4 @@
-const url = "http://127.0.0.1:3000/api/EDUCANDO/";
+const url = "http://34.230.250.81:3000/api/EDUCANDO/";
 let resultados = '';
 const formArticulo = document.querySelector("form");
 const dniedu = document.getElementById("DNIEDU");
